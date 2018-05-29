71cbee0cc9b3f7b6dc35fc5f3c22a4695f0c1399
ivanjo39191 <ivanjo39191@gmail.com>
2018-05-30 03:48:28 +0800

    v10

--- game/prod_settings.py
+++ game/prod_settings.py
@@ -1,5 +1,9 @@
 from .settings import *
 
+import dj_datebase_url
+DATABASES = {
+    'default': dj_datebase_url.config()
+}
 STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
