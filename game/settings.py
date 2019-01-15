3655d1632125f22563172bf82addec7ab7679db7
ivankao <ivanjo39191@gmail.com>
2019-01-15 15:53:58 +0800

    crawler v3

--- game/settings.py
+++ game/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!mkf01zn!zig#)@*th8ne&c*#6w$!ryt!vp)ej3%ep_6g2$r7*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
